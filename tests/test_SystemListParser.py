c827ac57c9427acf715abd55b5069cfdaa29109f
Geronimo Poppino <gpoppino@outlook.com>
2022-09-29 17:18:28 -0300

    Make unit test for SystemListParser more granular

--- tests/test_SystemListParser.py
+++ tests/test_SystemListParser.py
@@ -7,17 +7,23 @@ class TestSystemListParser(unittest.TestCase):
     def setUp(self):
         self.parser = SystemListParser("not-used-filename")
 
-    def testSystemListParser(self):
+    def test_systemListParserIsEmpty(self):
         self.assertDictEqual({}, self.parser.getSystems())
 
+    def test_systemListParserOneItem(self):
         self.parser._addSystem("instance-k3s-1.suse.local,2022-10-07 17:45:00")
         self.assertDictEqual(
             {"2022-10-07 17:45:00": ["instance-k3s-1.suse.local"]}, self.parser.getSystems())
 
+    def test_systemListParserAllDifferentDateItems(self):
+        self.parser._addSystem("instance-k3s-1.suse.local,2022-10-07 17:45:00")
         self.parser._addSystem("instance-k3s-2.suse.local,2022-10-08 17:50:00")
         self.assertDictEqual({"2022-10-07 17:45:00": ["instance-k3s-1.suse.local"], "2022-10-08 17:50:00": [
                              "instance-k3s-2.suse.local"]}, self.parser.getSystems())
 
+    def test_systemListParserSameDateItems(self):
+        self.parser._addSystem("instance-k3s-1.suse.local,2022-10-07 17:45:00")
+        self.parser._addSystem("instance-k3s-2.suse.local,2022-10-08 17:50:00")
         self.parser._addSystem("instance-k3s-3.suse.local,2022-10-07 17:45:00")
         self.assertDictEqual({"2022-10-07 17:45:00": ["instance-k3s-1.suse.local", "instance-k3s-3.suse.local"],
                              "2022-10-08 17:50:00": ["instance-k3s-2.suse.local"]}, self.parser.getSystems())
