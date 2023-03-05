ad2fc77b3c5093dd4b4da43ca1f5b93ee0cc1eb5
Geronimo Poppino <gpoppino@outlook.com>
2023-03-05 20:27:13 -0300

    Fix SystemErrateInspector tests

--- tests/test_SystemErrataInspector.py
+++ tests/test_SystemErrataInspector.py
@@ -2,12 +2,13 @@ import unittest
 from unittest.mock import Mock
 from susepatching import AdvisoryType
 from susepatching import SystemErrataInspector
+from susepatching import System
 
 
 class TestSystemErrataInspector(unittest.TestCase):
 
     def setUp(self):
-        pass
+        self.system = System("mysystem.suse.local")
 
     def test_hasSuggestedReboot(self):
         client = Mock()
@@ -15,7 +16,7 @@ class TestSystemErrataInspector(unittest.TestCase):
         client.errata.listKeywords.return_value = ['testing', 'having fun', 'reboot_suggested', "nothing"]
         client.system.getId.return_value = [{'id': '100100001'}]
 
-        systemErrataInspector = SystemErrataInspector(client, "mysystem.suse.local", AdvisoryType.ALL)
+        systemErrataInspector = SystemErrataInspector(client, self.system, AdvisoryType.ALL)
 
         self.assertTrue(systemErrataInspector.has_suggested_reboot())
 
@@ -25,15 +26,6 @@ class TestSystemErrataInspector(unittest.TestCase):
         client.errata.listKeywords.return_value = ['testing', 'having fun', "nothing", 'restart_suggested']
         client.system.getId.return_value = [{'id': '100100001'}]
 
-        systemErrataInspector = SystemErrataInspector(client, "mysystem.suse.local", AdvisoryType.ALL)
+        systemErrataInspector = SystemErrataInspector(client, self.system, AdvisoryType.ALL)
 
         self.assertFalse(systemErrataInspector.has_suggested_reboot())
-
-    def test_RaisesValueErrorNoSuchSystem(self):
-        client = Mock()
-        client.system.getId.return_value = []
-
-        systemErrataInspector = SystemErrataInspector(client, "mysystem.suse.local", AdvisoryType.ALL)
-
-        with self.assertRaises(ValueError):
-            systemErrataInspector.get_system_id()
